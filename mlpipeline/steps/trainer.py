8c1174683f5c0c276de9a875f36b66e25c4be788
kenghooi-teoh <tkenghooi@gmail.com>
2022-10-08 22:59:02 +0800

    refactor: fix typo

--- mlpipeline/steps/trainer.py
+++ mlpipeline/steps/trainer.py
@@ -8,7 +8,7 @@ SEED = 123
 
 
 class Trainer:
-    def __int__(self, x_train, x_val, y_val):
+    def __init__(self, x_train, x_val, y_val):
         self.x_train = x_train
         self.x_val = x_val
         self.y_val = y_val
