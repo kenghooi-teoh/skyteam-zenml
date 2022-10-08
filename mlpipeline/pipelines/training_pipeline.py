0032f2864485b79e216f2f2cd5be4756b0de61b4
kenghooi-teoh <tkenghooi@gmail.com>
2022-10-08 22:59:21 +0800

    refactor: update training_pipeline

--- mlpipeline/pipelines/training_pipeline.py
+++ mlpipeline/pipelines/training_pipeline.py
@@ -2,7 +2,7 @@ from zenml.integrations.constants import MLFLOW
 from zenml.pipelines import pipeline
 
 from ..steps.data_fetcher import DataFetcher
-from ..steps.dataset_maker import DatasetMaker
+from ..steps.data_preprocessor import DataPreprocessor
 from ..steps.trainer import Trainer
 from ..steps.feature_engineer import FeatureEngineer
 from ..steps.model_evaluator import ModelEvaluator
@@ -10,7 +10,6 @@ from ..steps.model_evaluator import ModelEvaluator
 
 class TrainingPipelineConfig:
     data_fetcher: DataFetcher
-    dataset_maker: DatasetMaker
     trainer: Trainer
     feature_engineer: FeatureEngineer
     evaluator: ModelEvaluator
@@ -19,10 +18,28 @@ class TrainingPipelineConfig:
 # TODO
 # - eventually write model to model registry with evaluation scores
 @pipeline(enable_cache=False, required_integrations=[MLFLOW])
-def training_pipeline(config: TrainingPipelineConfig):
+def training_pipeline():
     """
     Args:
 
     Returns:s
     """
     ...
+
+    train_df = DataFetcher().fetch_train_data()
+    val_df = DataFetcher().fetch_val_data()
+    label = DataFetcher().fetch_label_data()
+    print("data loaded")
+
+    train_feat = FeatureEngineer().feature_engineer(train_df)
+    val_feat = FeatureEngineer().feature_engineer(val_df)
+
+    x_train, train_y, x_val, y_val = DataPreprocessor().clean_data(train_feat, val_feat, label)
+    print("data preprocessed")
+
+    model = Trainer(x_train=x_train, x_val=x_val, y_val=y_val).train_xgb_model()
+    print("model trained")
+
+
+if __name__ == "main":
+    training_pipeline()
