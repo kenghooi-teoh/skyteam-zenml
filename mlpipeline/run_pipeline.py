04b6bf0e40a6d1bf85223cc90f9e040c29b9713d
kenghooi-teoh <tkenghooi@gmail.com>
2022-10-09 01:26:35 +0800

    update run_training_pipeline

--- mlpipeline/run_pipeline.py
+++ mlpipeline/run_pipeline.py
@@ -1,21 +1,23 @@
-from mlpipeline.pipelines.training_pipeline import training_pipeline
-from mlpipeline.steps.data_fetcher import fetch_train_data, fetch_val_data, fetch_label_data
-from mlpipeline.steps.data_preprocessor import clean_data
-# from mlpipeline.steps.trainer import train_xgb_model
-from mlpipeline.steps.feature_engineer import feature_engineer
+from pipelines.training_pipeline import training_pipeline
+from steps.data_fetcher import fetch_train_data, fetch_val_data, fetch_label_data
+from steps.data_preprocessor import clean_data
+from mlpipeline.steps.trainer import train_xgb_model
+from steps.feature_engineer import feature_engineer_train, feature_engineer_val
 
 
 def run_training_pipeline():
+    print("running pipeline")
     pipe = training_pipeline(
         fetch_train_data=fetch_train_data(),
         fetch_val_data=fetch_val_data(),
         fetch_label_data=fetch_label_data(),
-        feature_engineer=feature_engineer(),
+        feature_engineer_train=feature_engineer_train(),
+        feature_engineer_val=feature_engineer_val(),
         clean_data=clean_data(),
-        # train_xgb_model()
+        train_xgb_model=train_xgb_model()
     )
     pipe.run()
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     run_training_pipeline()
