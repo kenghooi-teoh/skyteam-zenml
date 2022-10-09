1ab0fdf1a1ac0a0db6915103bed6333c6e7ac3c4
yptheangel <choowilson93@gmail.com>
2022-10-09 17:39:33 +0800

    baseline code for evidently dashboard html render in streamlit

---
+++ streamlit/pages/03_perf_check.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from utils import add_logo
+
+add_logo()
+st.markdown("# Performance Checks")
+st.sidebar.markdown("# Performance Checks")
+
+
+
+
+
