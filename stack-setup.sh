04e4fd92febb630491f91d03c1cced61c3bf234d
kenghooi <tkenghooi@gmail.com>
2022-10-10 12:39:37 +0800

    fix: remove extra s

--- stack-setup.sh
+++ stack-setup.sh
@@ -1,4 +1,4 @@
 zenml integration install mlflow sklearn xgboost -y
 
 zenml experiment-tracker register mlflow_tracker --flavor=mlflow
-zenml stack update default -e mlflow_trackers
+zenml stack update default -e mlflow_tracker
