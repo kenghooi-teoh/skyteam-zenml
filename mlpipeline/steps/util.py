6251efd2b19762454080455647ba7121e1758cca
kenghooi-teoh <tkenghooi@gmail.com>
2022-10-16 23:02:39 +0800

    add steps/util/raw_pred_to_class

--- mlpipeline/steps/util.py
+++ mlpipeline/steps/util.py
@@ -28,3 +28,7 @@ def amex_metric_mod(y_true, y_pred):
         gini[i] = np.sum((lorentz - weight_random) * weight)
 
     return 0.5 * (gini[1] / gini[0] + top_four)
+
+
+def raw_pred_to_class(pred: float):
+    return int(pred >= 0.5)
